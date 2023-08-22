48fdc4a1f76efbf2e01e43096dab55e9c176a675
Joe Cooler <joe_cooler@msn.com>
2023-08-21 20:18:37 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    <img src='src/assets/avataaars.svg' />
+    <img src='../src/assets/avataaars.svg' />
       <h2>Joseph Cooler</h2>
       <p>Full Stack React Developer<br></br>
         Wisconsin, USA</p>
