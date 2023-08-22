dba1fc6f57078f7dc9c4e4c62bcef8ebe855cfa7
Joe Cooler <joe_cooler@msn.com>
2023-08-21 20:13:50 -0500

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Test</h1>
+    <img src='src/assets/avataaars.svg' />
+      <h2>Joseph Cooler</h2>
+      <p>Full Stack React Developer<br></br>
+        Wisconsin, USA</p>
     </>
   )
 }
